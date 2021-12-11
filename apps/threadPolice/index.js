9e9b3147d8da3351fe6fdb8a95c479fe59784148
rhenium <rhenium750@gmail.com>
2021-12-11 10:07:59 +0900

    !addMsgコマンドを追加

--- apps/threadPolice/index.js
+++ apps/threadPolice/index.js
@@ -3,6 +3,32 @@ const _ = require("lodash");
 exports.call = async (client) => {
   client.on("messageCreate", async (msg) => {
     if (msg.author.bot) return;
+
+    if (_.startsWith(msg.content, "!addMsg") && msg.channel.isThread()) {
+      try {
+        const command = _.split(msg.content, " ");
+        const targetMsgId = command[1];
+        const targetMsg = await msg.channel.parent.messages.fetch(targetMsgId);
+
+        await msg.channel.send({
+          embeds: [
+            {
+              author: {
+                name: targetMsg.member.displayName,
+                icon_url: targetMsg.member.displayAvatarURL(),
+              },
+              description: targetMsg.content,
+              color: targetMsg.author.hexAccentColor,
+              timestamp: targetMsg.createdAt,
+            },
+          ],
+        });
+        await targetMsg.delete();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
     if (msg.type === "REPLY" && _.includes(msg.channel.topic, "スレッド警察")) {
       try {
         const targetMsg = await msg.channel.messages.fetch(
@@ -10,7 +36,8 @@ exports.call = async (client) => {
         );
 
         if (!targetMsg.hasThread) {
-          const threadNameValid = !targetMsg.content && targetMsg.content != "";
+          const threadNameValid =
+            !!targetMsg.content && targetMsg.content != "";
           await targetMsg.startThread({
             name: threadNameValid ? targetMsg.content : "thread",
           });
@@ -25,7 +52,8 @@ exports.call = async (client) => {
                   icon_url: msg.member.displayAvatarURL(),
                 },
                 description: msg.content,
-                color: 4303284,
+                color: msg.author.hexAccentColor,
+                timestamp: msg.createdAt,
               },
             ],
           });
